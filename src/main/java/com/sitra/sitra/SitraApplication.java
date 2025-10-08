ed8ab8d7cb7a1e4d2dd012df9d399905439d1bc8
juan-23-08 <juan.ignacio.27@unsch.edu.pe>
2025-10-07 21:27:15 -0500

    Initial Proyect

---
+++ src/main/java/com/sitra/sitra/SitraApplication.java
@@ -0,0 +1,13 @@
+package com.sitra.sitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitra/sitra/SitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitra.sitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
