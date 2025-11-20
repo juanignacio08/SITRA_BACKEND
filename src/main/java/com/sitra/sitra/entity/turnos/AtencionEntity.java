a36761509b16f185b9d92cfe23c926039eaf4bce
juan-23-08 <juan.ignacio.27@unsch.edu.pe>
2025-11-19 22:05:09 -0500

    feat(Atencion): Se agrego la entidad de Atencion.

---
+++ src/main/java/com/sitra/sitra/entity/turnos/AtencionEntity.java
@@ -0,0 +1,69 @@
+package com.sitra.sitra.entity.turnos;
+
+import com.sitra.sitra.entity.seguridad.UsuarioEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "atencion", schema = "turnos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AtencionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "atencionid")
+    private Long atencionId;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDate fecha;
+
+    @Column(name = "horainicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "horafin", nullable = false)
+    private LocalTime horaFin;
+
+    @Column(name = "codventanilla", length = 6, nullable = false)
+    private String codVentanilla;
+
+    @Column(name = "observacion")
+    private String observacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "asesorid", nullable = false)
+    private UsuarioEntity asesor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ordenatencionid", nullable = false)
+    private OrdenAtencionEntity ordenAtencion;
+
+    @Column(name = "codestadoatencion", length = 6, nullable = false)
+    private String codEstadoAtencion;
+
+    @Column(name = "estado", nullable = false)
+    private int estado;
+
+    @Column(name = "actualizadopor", nullable = false)
+    private Long actualizadoPor;
+
+    @Column(name = "fechaactualizacion", nullable = false)
+    private LocalDateTime fechaActualizacion;
+
+    @Column(name = "creadopor", nullable = false)
+    private Long creadoPor;
+
+    @Column(name = "fechacreacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "eliminado", nullable = false)
+    private Boolean eliminado = Boolean.FALSE;
+
+}
